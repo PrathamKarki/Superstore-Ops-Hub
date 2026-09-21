c0a3ce8a6f000714acd0241e4ae053ce36693e9d
Pratham Karki <you@example.com>
2026-09-21 11:27:22 +0545

    Fix: typo fix while creating inventory schema fixed again

--- sql/01_Schema/07_Create_inventory_table.sql
+++ sql/01_Schema/07_Create_inventory_table.sql
@@ -3,10 +3,10 @@
 CREATE table inventory(
     product_id VARCHAR2(30) NOT NULL, 
     qty_on_hand NUMBER(10) NOT NULL, 
-    reoder_point NUMBER(10) DEFAULT 50 NOT NULL, 
+    reorder_point NUMBER(10) DEFAULT 50 NOT NULL, 
     last_updated TIMESTAMP DEFAULT SYSTIMESTAMP NOT NULL, 
     constraint pk_inventory PRIMARY KEY(product_id),
     constraint fk_inventory FOREIGN KEY(product_id) REFERENCES products(product_id),
     constraint  chk_inventory_quantity CHECK(qty_on_hand >= 0),
-    constraint chk_inventory_reorder CHECK(reoder_point > 0)
+    constraint chk_inventory_reorder CHECK(reorder_point > 0)
 );
